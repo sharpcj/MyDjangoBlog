2926fb8b17b450af45614e21893f5116da645264
sharpcj <li_joy99@163.com>
2017-09-07 23:11:27 +0800

    yeh! use templates

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,14 +1,36 @@
+from datetime import datetime
+
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.template.loader import get_template
+
 from mainsite.models import Post
 
 
 def homepage(request):
+    """   not use templates
     posts = Post.objects.all()
     post_lists = list()
     for count, post in enumerate(posts):
         post_lists.append("No.{}:".format(str(count)) + str(post.title) + "<br>")
         post_lists.append("<small>" + str(post.body.encode('utf-8')) + "</small><br><br>")
     return HttpResponse(post_lists)
+    """
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
+
+
+def showpost(request, slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
