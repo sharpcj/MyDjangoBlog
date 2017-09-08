c20fe3170683d6a9e849e835820831be5faaaea5
sharpcj <li_joy99@163.com>
2017-09-08 22:20:42 +0800

    use staticfiles

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,9 +1,12 @@
+# coding:utf-8
+from __future__ import unicode_literals    #  为了兼容python2 ① 这个必须放在第一行
 from django.db import models
-
 # Create your models here.
 from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible       #  为了兼容python2 ②
 
 
+@python_2_unicode_compatible                #  为了兼容python2 ③
 class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.CharField(max_length=200)
@@ -13,5 +16,5 @@ class Post(models.Model):
     class Meta:
         ordering = ('-pub_date',)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
